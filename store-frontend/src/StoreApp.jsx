2aed013c4c158883cbe40a46e39ac8293bf2b11c
lyongaming <owlsmouth@gmail.com>
2022-06-23 12:14:07 -0400

    Starting Frontend side

---
+++ store-frontend/src/StoreApp.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const StoreApp = () => <div>ª</div>;
---
+++ store-frontend/src/index.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import { StoreApp } from "./StoreApp";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<StoreApp />);
