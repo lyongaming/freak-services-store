832d1c41a2e4f0679b9702d8f243ccbbeb68bcf3
lyongaming <owlsmouth@gmail.com>
2022-06-24 15:06:08 -0400

    Animated Background added

--- store-frontend/src/StoreApp.jsx
+++ store-frontend/src/StoreApp.jsx
@@ -1,3 +1,28 @@
 import React from 'react';
+import { SquaresBackground } from './SquaresBackground';
 
-export const StoreApp = () => <div>ª</div>;
+export const StoreApp = () => {
+
+    return (
+        <>
+
+            {/* <section>
+                <div className="deck">
+                    <h3>User Register</h3>
+                    <div className="hand">
+                        <div className="userCard">
+                            <img src="leon no sleep.jpg" alt="im dying" />
+                        </div>
+                        <div className="content">
+                            <h2 className="rank"><small> &gt;&gt; </small></h2>
+                            <h4>The Lion</h4>
+                        </div>
+                    </div>
+                </div>
+            </section> */}
+            
+            <SquaresBackground />
+        </>
+    );
+
+}
