2a2f3092d941a8b17a9b0f0ed9c4c0675d492c5d
lyongaming <owlsmouth@gmail.com>
2022-06-30 08:02:53 -0400

    Refactoring the background

--- frontend-store/src/StoreApp.jsx
+++ frontend-store/src/StoreApp.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { PageBackground } from './hocs/PageBackground';
 
 import { LoginPage } from "./pages/LoginPage";
+import { ServicesPage } from "./pages/ServicesPage";
 
 import "./style.css";
 
@@ -10,7 +12,8 @@ export const StoreApp = () => {
     return (
         <Router>
             <Routes>
-                <Route path='/login' element={ <LoginPage /> } />
+                <Route path='/login' element={ <PageBackground><LoginPage /></PageBackground> } />
+                <Route path='/services' element={ <PageBackground><ServicesPage /></PageBackground> } />
             </Routes>
         </Router>
     );
