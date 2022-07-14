2e9556aaf32368ba8d3659c5aa8e8bc4c8d53724
lyongaming <owlsmouth@gmail.com>
2022-07-14 05:12:27 -0400

    Adding mouse-based movement to the 404 page

--- frontend-store/src/pages/NotFound.jsx
+++ frontend-store/src/pages/NotFound.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 import "./error.css";
 
 export const NotFound = () => {
 
+  const backgroundRef = useRef();
   const navigate = useNavigate();
 
   const goToHome = () => navigate("/");
 
+  const moveBackground = ({ clientX, clientY }) => {
+    const x = - clientX/5; 
+    const y = - clientY/5;
+    backgroundRef.current.style.backgroundPositionX = x + 'px';
+    backgroundRef.current.style.backgroundPositionY = y + 'px';
+  }
+
   return (
-    <div id="container">
+    <div id="container" ref={ backgroundRef } onMouseMove={ moveBackground }>
       <div class="content">
         <h2>404</h2>
         <h4>Opps! page not found</h4>
