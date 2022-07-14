92d89fadeafd3c29ce6dcc2b8d648d17acaa1c1e
lyongaming <owlsmouth@gmail.com>
2022-07-14 08:26:53 -0400

    Fixing the navbar

--- frontend-store/src/components/Navbar/Navbar.jsx
+++ frontend-store/src/components/Navbar/Navbar.jsx
@@ -5,18 +5,26 @@ import "./Navbar.css";
 
 export const Navbar = () => {
   return (
-    <nav>
-        <ul class="nav-bar">
-            <li><NavLink to="/login" className={({ isActive }) => 
-              isActive ? "link active" : "link"
-            }>register</NavLink></li>
-            <li><NavLink to="/services" className={({ isActive }) => 
-              isActive ? "link active" : "link"
-            }>buy now</NavLink></li>
-            <li><NavLink to="/" className={({ isActive }) => 
-              isActive ? "link active" : "link"
-            }>home</NavLink></li>
-        </ul> 
-    </nav>
+    <header>
+      <nav>
+          <ul class="nav-bar">
+              <li><NavLink to="/" className={({ isActive }) => 
+                isActive ? "link active" : "link"
+              }>home</NavLink></li>
+              <li><NavLink to="/services" className={({ isActive }) => 
+                isActive ? "link active" : "link"
+              }>buy now</NavLink></li>
+              <li><NavLink to="/historial" className={({ isActive }) => 
+                isActive ? "link active" : "link"
+              }>historial</NavLink></li>
+              <li><NavLink to="/invoices" className={({ isActive }) => 
+                isActive ? "link active" : "link"
+              }>invoices</NavLink></li>
+          </ul> 
+      </nav>
+          <NavLink to="/login" className={({ isActive }) => 
+            isActive ? "login active" : "login"
+          }>login</NavLink>
+    </header>
   );
 }
--- frontend-store/src/pages/landing_page/LandingPage.jsx
+++ frontend-store/src/pages/landing_page/LandingPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+
+import "./LandingPage.css";
 
 export const LandingPage = () => {
   return (
     <>
-      
+      <div class="content">
+        <h1>pay your subscriptions.</h1>
+        <p>automatically with us</p>
+      </div>
     </>
-  )
-}
+  );
+};
