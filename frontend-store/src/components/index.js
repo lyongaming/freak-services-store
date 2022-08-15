f4427f6d9345ea470142a874d511d7c5bcc99b75
lyongaming <owlsmouth@gmail.com>
2022-08-14 22:59:56 -0400

    ServiceForm component created and added to ServicePage

--- frontend-store/src/components/index.js
+++ frontend-store/src/components/index.js
@@ -1,8 +1,10 @@
+import { ServicesForm } from "./ServicesForm/ServicesForm";
 import { UserCard } from "./UserCard/UserCard";
 import { UserOption } from "./UserOption/UserOption";
 import { Navbar } from "./Navbar/Navbar";
 
 export {
+    ServicesForm,
     UserCard,
     UserOption,
     Navbar
--- frontend-store/src/pages/services/ServicesPage.jsx
+++ frontend-store/src/pages/services/ServicesPage.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 
-import { UserCard } from "../../components/UserCard/UserCard";
+import { UserCard, ServicesForm } from "../../components";
 
 import "./ServicePage.css";
-import "./buttom.css";
-import "./formservice.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faShoppingCart, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export const ServicesPage = () => {
   return (
@@ -14,32 +10,7 @@ export const ServicesPage = () => {
       <div className="service-page_container">
         <UserCard />
       </div>
-
-      <div className="wrapper">
-        <header>Services</header>
-        <div className="inputField">
-          <input type="text" list="services" placeholder="service" />
-          <datalist id="services">
-            <option value="Xbox Game Pass"></option>
-            <option value="Play Station +"></option>
-            <option value="Club Fortnite"></option>
-            <option value="Netflix"></option>
-            <option value="Amazon prime"></option>
-            <option value="Ninstendo switch online"></option>
-          </datalist>
-          <button>
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
-        </div>
-        <ul className="allList">
-          {/* <li>alalalalalllalala  <span><i className="fas fa-trash"></i></span> </li> */}
-          {/* <li>alalalalalllalala  <span><i className="fas fa-trash"></i></span> </li> */}
-        </ul>
-        <div className="footer">
-            <button style={ { "--clr": "rgb(16,124,248)" } } className="modal__button" id="open-modal"><FontAwesomeIcon icon={ faShoppingCart } /></button>
-            <button className="deleteAll" style={ { "--clr": "#e74c3c" } }><FontAwesomeIcon icon={ faTrash } /></button>
-        </div>
-      </div>
+      <ServicesForm />
     </>
   );
 };
